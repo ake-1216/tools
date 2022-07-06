687366413bce29505f6b161f89a05772c73e02b8
阿珂 <2454345235@qq.com>
2022-07-06 13:22:42 +0800

    add diffForhumans function

--- src/Laravel/function.php
+++ src/Laravel/function.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Storage;
 
 if (!function_exists('disk')) {
@@ -17,5 +18,20 @@ if (!function_exists('disk')) {
         $disk = $disk ?: config('admin.upload.disk');
         $storage = Storage::disk($disk);
         return $storage->url($content);
+    }
+}
+
+if (!function_exists('diffForHumans')){
+    /**
+     * @description:获取当前时间，距离现在的时间
+     * @param $time
+     * @return string
+     * @Author:AKE
+     * @Date:2022/7/6 13:22
+     */
+    function diffForHumans($time)
+    {
+        $dt = Carbon::create($time);
+        return $dt->diffForHumans();
     }
 }
