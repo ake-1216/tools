6dd00f120c2c85030526a58c160018f5d0935f59
阿珂 <2454345235@qq.com>
2022-08-05 15:07:49 +0800

    添加汉字排序方法

--- src/Laravel/Base/Model.php
+++ src/Laravel/Base/Model.php
@@ -10,6 +10,7 @@ use Dcat\Admin\Traits\HasDateTimeFormatter;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model as Base;
+use Illuminate\Support\Facades\DB;
 
 class Model extends Base
 {
@@ -37,5 +38,18 @@ class Model extends Base
     public function scopePublish($query)
     {
         $query->where('is_publish', 1);
+    }
+
+    /**
+     * @description:汉字排序
+     * @param $query
+     * @param string $field 需要排序字段名
+     * @param string $order 排序规则
+     * @Author:AKE
+     * @Date:2022/2/21 14:54
+     */
+    public function scopeGbkOrder($query, string $field, string $order = 'asc')
+    {
+        $query->orderBy(DB::raw("convert(`{$field}` using gbk)"), $order);
     }
 }
